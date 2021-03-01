edc6cbfb1df3f15c18d4ddebccaa436282dec00b
Edis Hulpea <edis.hulpea@gmail.com>
2021-03-01 13:05:19 +0200

    first draft

--- jscript.js
+++ jscript.js
@@ -5,19 +5,31 @@ $(".btnBackTop").on("click", function () {
   return false;
 });
 
-//Stats Counter
-
-$(".counter").each(function () {
-  $(this)
-    .prop("counter", 0)
-    .animate(
-      { counter: $(this).text() },
-      {
-        duration: 10000,
-        easing: "linear",
-        step: function () {
-          $(this).text(Math.ceil(this.counter));
-        },
-      }
-    );
-});
+// Scrolling text;
+$(window).scroll(testScroll);
+let viewed = false;
+function isScrolledIntoView(elem) {
+  const docViewTop = $(window).scrollTop();
+  const docViewBottom = docViewTop + $(window).height();
+  const elemTop = $(elem).offset().top;
+  const elemBottom = elemTop + $(elem).height();
+  return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
+function testScroll() {
+  if (isScrolledIntoView($("#check-out-stats")) && !viewed) {
+    viewed = true;
+    $(".counter").each(function () {
+      $(this)
+        .prop("counter", 0)
+        .animate(
+          { counter: $(this).text() },
+          {
+            duration: 3000,
+            step: function () {
+              $(this).text(Math.ceil(this.counter));
+            },
+          }
+        );
+    });
+  }
+}
